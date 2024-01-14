95919a45860a7646490ac9140aed690d3435ae3c
hhhanqian <44803779+hhhanqian@users.noreply.github.com>
2024-01-14 11:46:08 +0800

    web_demo path

--- code/InternLM/web_demo.py
+++ code/InternLM/web_demo.py
@@ -79,8 +79,8 @@ def main():
     model, tokenizer = load_model()
     print("load model end.")
 
-    user_avator = "doc/imgs/user.png"
-    robot_avator = "doc/imgs/robot.png"
+    user_avator = "/home/xlab-app-center/code/InternLM/doc/imgs/user.png"
+    robot_avator = "/home/xlab-app-center/code/InternLM/doc/imgs/robot.png"
 
     st.title("InternLM-Chat-7B")
 
